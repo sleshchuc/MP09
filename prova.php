d6d8561987c19e2d39da74c41153bafac9699890
sleshuck <alumne@insebre-com>
2023-10-16 18:44:46 +0200

    afegir index

--- prova.php
+++ prova.php
@@ -1,2 +1,13 @@
 <?php
-echo "hello world"
+$txt = "hello world";
+echo $txt;
+echo "</br>";
+
+$a = 612;
+$b = 40;
+$c = 0.9994;
+$x = 61;
+$y = 9162;
+$z = $a - $b % $x + $y + $c;
+echo $z;
+echo "</br>";
